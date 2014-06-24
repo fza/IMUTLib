ee397d29a7824a86bec2211cb0131f6d20ffc776
Felix Zandanel <felix@zandanel.me>
2014-06-24 10:36:07 +0200

    Initial Commit

---
+++ IMUT/IMUT.h
@@ -0,0 +1,13 @@
+//
+//  IMUT.h
+//  IMUT
+//
+//  Created by fza on 24.06.14.
+//  Copyright (c) 2014 Felix Zandanel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IMUT : NSObject
+
+@end
---
+++ IMUT/IMUT.m
@@ -0,0 +1,13 @@
+//
+//  IMUT.m
+//  IMUT
+//
+//  Created by fza on 24.06.14.
+//  Copyright (c) 2014 Felix Zandanel. All rights reserved.
+//
+
+#import "IMUT.h"
+
+@implementation IMUT
+
+@end
